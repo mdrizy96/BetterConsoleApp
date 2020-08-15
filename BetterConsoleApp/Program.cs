ee8926a65b706b40118164ca5a48a8535f028d63
mdrizy96 <duncanokeno@gmail.com>
2020-08-15 05:31:32 -0700

    Configure console app for logging, dependency injection, and settings

--- BetterConsoleApp/Program.cs
+++ BetterConsoleApp/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+// DI, Logging with Serilog, and use AppSettings
 
 namespace BetterConsoleApp
 {
@@ -6,7 +12,33 @@ namespace BetterConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Application starting");
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+
+                })
+                .UseSerilog()
+                .Build();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder
+                // .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
         }
     }
 }
